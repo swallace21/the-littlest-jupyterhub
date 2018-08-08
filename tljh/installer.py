c82d062f374123078afbda685e3b765fde7de232
Shaun Wallace <shaun_wallace@brown.edu>
2018-08-08 14:38:46 -0400

    node check test

--- tljh/installer.py
+++ tljh/installer.py
@@ -89,9 +89,9 @@ sckuXINIU3DFWzZGr0QrqkuE/jyr7FXeUJj9B7cLo+s/TXo+RaVfi3kOc9BoxIvy
 =CLGF
 -----END PGP PUBLIC KEY BLOCK-----
     """.strip()
-    apt.trust_gpg_key(key)
-    apt.add_source('nodesource', f'https://deb.nodesource.com/node_10.x', 'main')
-    apt.install_packages(['nodejs'])
+    #apt.trust_gpg_key(key)
+    #apt.add_source('nodesource', f'https://deb.nodesource.com/node_10.x', 'main')
+    #apt.install_packages(['nodejs'])
 
 
 def ensure_chp_package(prefix):
