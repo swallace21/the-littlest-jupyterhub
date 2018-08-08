7748e150f6e18cb7a0b2aaed4ddb10180dc95821
Shaun Wallace <shaun_wallace@brown.edu>
2018-08-08 14:36:32 -0400

    UNIT Test check if node exists

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -6,5 +6,9 @@ import os
 
 
 def test_ensure_node():
-    installer.ensure_node()
-    assert os.path.exists('/usr/bin/node')
+    if os.path.exists("/home/el/myfile.txt"):
+        pass
+    else:
+        installer.ensure_node()
+        node_file_path = '/usr/bin/node'
+        assert os.path.exists(node_file_path), "node not found in: %r" % node_file_path 
