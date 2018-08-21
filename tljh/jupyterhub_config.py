6acf14952a42f727069f440865cb733116e8b6b5
Shaun Wallace <shaun_wallace@brown.edu>
2018-08-21 15:46:39 -0400

    Disable terminal for normal users

--- tljh/jupyterhub_config.py
+++ tljh/jupyterhub_config.py
@@ -17,8 +17,16 @@ class CustomSpawner(SystemdSpawner):
         Perform system user activities before starting server
         """
         # FIXME: Move this elsewhere? Into the Authenticator?
-        system_username = 'jupyter-' + self.user.name
+        #system_username = 'jupyter-' + self.user.name
+        system_username = self.user.name
         user.ensure_user(system_username)
+
+        # only allow certain users to access terminal through jupyterhub web interface
+        if (self.user.name == 'dpotter'):
+            self.default_shell = '/bin/bash'
+        else:
+            self.default_shell = '/bin/false'
+
         user.ensure_user_group(system_username, 'jupyterhub-users')
         if self.user.admin:
             user.ensure_user_group(system_username, 'jupyterhub-admins')
@@ -26,19 +34,19 @@ class CustomSpawner(SystemdSpawner):
             user.remove_user_group(system_username, 'jupyterhub-admins')
         return super().start()
 
-
 c.JupyterHub.spawner_class = CustomSpawner
 
 # Use a high port so users can try this on machines with a JupyterHub already present
 c.JupyterHub.hub_port = 15001
 
-c.ConfigurableHTTPProxy.should_start = False
-c.ConfigurableHTTPProxy.api_url = 'http://127.0.0.1:15002'
+#c.ConfigurableHTTPProxy.should_start = False
+#c.ConfigurableHTTPProxy.api_url = 'http://127.0.0.1:15002'
 
 c.SystemdSpawner.extra_paths = [os.path.join(USER_ENV_PREFIX, 'bin')]
-c.SystemdSpawner.default_shell = '/bin/bash'
+# c.SystemdSpawner.default_shell = '/bin/bash' #sw90 - now done per spawn
 # Drop the '-singleuser' suffix present in the default template
-c.SystemdSpawner.unit_name_template = 'jupyter-{USERNAME}'
+#c.SystemdSpawner.unit_name_template = 'jupyter-{USERNAME}'
+c.SystemdSpawner.unit_name_template = '{USERNAME}'
 
 config_overrides_path = os.path.join(INSTALL_PREFIX, 'config.yaml')
 if os.path.exists(config_overrides_path):
